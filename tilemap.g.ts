654758f0af692863e6718fa3fa4d53ea157b7b34
hectorelmas <124789223+hmorhid1407@users.noreply.github.com>
2023-04-26 13:09:42 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ tilemap.g.ts
@@ -0,0 +1,14 @@
+// Código generado automáticamente. No editar.
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency18 = image.ofBuffer(hex``);
+
+    helpers._registerFactory("tile", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "transparency18":return transparency18;
+        }
+        return null;
+    })
+
+}
+// Código generado automáticamente. No editar.
